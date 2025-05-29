165c9c196be4a1d1c79b9d7174983272ba93d24e
Abdul Hakeem <abdulhakeem080255@gmail.com>
2025-05-29 13:27:38 +0100

    fix: convert connect wallet button to that from dApp

--- frontend/auction-dapp/src/Layout.tsx
+++ frontend/auction-dapp/src/Layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import { Navigation } from "./components/Navigation";
 
 const Layout = ({
@@ -9,6 +10,7 @@ const Layout = ({
     <div>
       <Navigation />
       {children}
+      <Footer />
     </div>
   );
 };
--- frontend/auction-dapp/src/components/Footer.tsx
+++ frontend/auction-dapp/src/components/Footer.tsx
@@ -1,5 +1,32 @@
+import { FaGithub, FaTwitter } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="border-t border-[#006fee]/20">
+      <div className="container py-4 flex flex-col md:flex-row justify-between items-center">
+        <div className="flex items-center">
+          <Link to="/" className="text-xl font-bold gradient-text">
+            NFTVerse
+          </Link>
+        </div>
+        <div className="flex justify-end items-center gap-3 text-gray-600">
+          <Link
+            to="https://www.x.com"
+            className="p-2 rounded-full border border-gray-400 hover:bg-gray-100"
+          >
+            <FaTwitter size={20} />
+          </Link>
+          <Link
+            className="p-2 rounded-full border border-gray-400 hover:bg-gray-100"
+            to="https://github.com/Akpahsamuel/Auction"
+          >
+            <FaGithub size={20} />
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
